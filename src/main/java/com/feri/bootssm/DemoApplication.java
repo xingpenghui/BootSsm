d5173e17b22a5ce26d6a9cee315b67b1a4254af1
xing <xingfei_work@163.com>
2019-10-21 16:40:58 +0800

    添加Druid的SQL监控和Swagger接口文档

--- src/main/java/com/feri/bootssm/DemoApplication.java
+++ src/main/java/com/feri/bootssm/DemoApplication.java
@@ -3,13 +3,23 @@ package com.feri.bootssm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.ServletContextApplicationContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication //注解 标记SpringBoot项目的开关类
 @MapperScan("com.feri.bootssm.dao") //Mybatis的dao层扫描
-public class DemoApplication {
+@EnableSwagger2 //启用Swagger
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 }
--- src/main/java/com/feri/bootssm/controller/WeatherController.java
+++ src/main/java/com/feri/bootssm/controller/WeatherController.java
@@ -3,6 +3,9 @@ package com.feri.bootssm.controller;
 import com.feri.bootssm.entity.Weather;
 import com.feri.bootssm.service.WeatherService;
 import com.feri.bootssm.vo.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-10-21 10:48
  */
 @RestController
+@Api(value = "实现天气的各种操作",tags = "实现天气的各种操作")
 public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
     //新增
+    @ApiOperation(value = "实现天气的新增",notes = "实现天气的新增")
     @PostMapping("api/weather/save.do")
-    public R save(@RequestBody Weather weather){
+    public R save(@ApiParam(value = "天气信息的JSON字符串") @RequestBody Weather weather){
         return weatherService.save(weather);
     }
     //查询
+    @ApiOperation(value = "实现全部天气信息的查询",notes = "实现全部天气信息的查询")
     @GetMapping("api/weather/queryall.do")
     public R all(){
         return weatherService.queryAll();
